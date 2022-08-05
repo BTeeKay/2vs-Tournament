ddca0de1b70a83ecd119780c1a436eb0a80b44d0
Brian Kerr <briantkerr91@gmail.com>
2022-08-05 10:34:14 +0100

    Added client react app and server spring boot frameworks

---
+++ server/2vs.tournament/src/test/java/com/vs/vs/tournament/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vs.vs.tournament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
