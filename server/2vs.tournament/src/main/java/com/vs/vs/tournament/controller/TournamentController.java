93df801c2f7ab2e90b64891d71bafe94ef7256df
Brian Kerr <briantkerr91@gmail.com>
2022-08-09 11:11:15 +0100

    Cors should now be enabled

--- server/2vs.tournament/src/main/java/com/vs/vs/tournament/controller/TournamentController.java
+++ server/2vs.tournament/src/main/java/com/vs/vs/tournament/controller/TournamentController.java
@@ -7,11 +7,13 @@ import com.vs.vs.tournament.repository.TournamentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 @RestController
+@CrossOrigin
 public class TournamentController {
 
     @Autowired
