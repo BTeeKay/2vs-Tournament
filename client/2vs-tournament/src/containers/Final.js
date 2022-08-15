119822f9f001ecb726598aeed8239f8a316d0492
Brian Kerr <briantkerr91@gmail.com>
2022-08-15 16:36:44 +0100

    started funct of saving tournament but need a merge first@

--- client/2vs-tournament/src/containers/Final.js
+++ client/2vs-tournament/src/containers/Final.js
@@ -3,9 +3,12 @@ import Game from "../components/Game/Game";
 
 const Final = ({ finalists }) => {
 
+    function handleFinalGame() {
+        console.log("Picked a winner")
+    }
     return (
         <div id="round">
-            <Game name={"Final"} player1={finalists[0]} player2={finalists[1]} />
+            <Game name={"Final"} player1={finalists[0]} player2={finalists[1]} handleFinalGame={handleFinalGame}/>
         </div>
     )
 
